7ee478956620a526048bf0ad643e403529434746
jewbre <jewbre18@gmail.com>
2018-03-04 20:44:11 +0100

    Person base classπ

--- base/Person.ts
+++ base/Person.ts
@@ -2,9 +2,6 @@ import { Place } from "./Place";
 
 export abstract class Person {
     name: string;
-    age: number;
-    energy: number;
-    state: string;
 
     think(thought: string) {
         console.log(`
@@ -12,24 +9,7 @@ ${name} is thinking: ${thought}
         `);
     }
 
-    abstract canWalk(): boolean;
-
-    abstract walk(): void;
-
-    abstract wakeUp(): void;
-
-    abstract cry(): void;
-
-    abstract smile(): void;
-
-    abstract turnSad(): void;
-
     abstract goTo(place: Place): void;
 
     abstract goToWork(): void;
-
-    abstract sleep(numberOfHours: number): void;
-
-    abstract work(): void;
-
 }
